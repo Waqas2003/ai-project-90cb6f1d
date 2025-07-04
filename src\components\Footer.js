1782602b5e7b7b487990ee626dee7d5d17e5465f
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 23:22:53 +0500

    Add src\components\Footer.js

---
+++ src\components\Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2023 Pakistan Atomic Power</p>
+        </footer>
+    );
+};
+
+export default Footer;
