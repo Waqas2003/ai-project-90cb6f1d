b172f87b79731e11975384b2dcf5e352b8275939
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 23:22:49 +0500

    Add src\components\About.js

---
+++ src\components\About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../Header';
+import Footer from '../Footer';
+
+const About = () => {
+    return (
+        <div>
+            <Header />
+            <main>
+                <h1>About Us</h1>
+                <p>
+                    Pakistan Atomic Power is a leading organization in the field of nuclear energy in Pakistan.
+                </p>
+            </main>
+            <Footer />
+        </div>
+    );
+};
+
+export default About;
